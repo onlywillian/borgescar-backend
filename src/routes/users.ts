d6d21d1e8e4e49af062ce19f5405c3ed3f8a1921
onlywillian <willianandrade953@gmail.com>
2023-02-05 09:45:35 -0300

    users crud complete

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -9,7 +9,7 @@ router.get('/users/all', async (req: Request, res: Response) => {
 
     const users = await prisma.user.findMany();
 
-    if (!users) return res.send({ Error: "users not found" }).status(400)
+    if (!users) return res.json({ Error: "users not found" }).status(400)
 
     return res.json(users).status(200)
 })
@@ -18,16 +18,35 @@ router.post('/users/new', async (req: Request, res: Response) => {
     const users = await prisma.user.create({
         data: req.body
     });
+    ''
+    if (!users) return res.json({ Error: "User is not created" }).status(500)
 
-    return res.send({ Users: users })
+    return res.json({ OK: "Query success", Users: users }).status(200)
 })
 
-router.put('/users/update', (req: Request, res: Response) => {
+router.put('/users/update', async (req: Request, res: Response) => {
+    const { oldEmail, newEmail } = req.body
 
+    const updatedUser = await prisma.user.update({
+        where: {
+            email: oldEmail
+        },
+        data: {
+            email: newEmail
+        }
+    })
+
+    return res.json(updatedUser).status(200)
 })
 
-router.delete('/users/remove', (req: Request, res: Response) => {
-    
+router.delete('/users/remove', async (req: Request, res: Response) => {
+    const { email } = req.body
+
+    const deletedUser = await prisma.user.delete({
+        where: {
+            email: email
+        }
+    })
 })
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import router from './routes/users';
+import usersRouter from './routes/users';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(router);
+app.use(usersRouter);
 
 app.listen(3000);
