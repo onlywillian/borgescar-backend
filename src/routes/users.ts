a39e0c47e6655e7a88fed29cad0eac318593211d
onlywillian <willianandrade953@gmail.com>
2023-04-14 23:18:45 -0300

    Exchanging json method with the send method

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -7,10 +7,10 @@ const prisma = new PrismaClient()
 router.get('/users/all', async (req: Request, res: Response) => {
     const users = await prisma.user.findMany();
 
-    if (!users) return res.json({ Error: "users not found" }).status(400)
+    if (users.length === 0) return res.send({ Error: "users not found" }).status(404);
 
-    return res.json(users).status(200)
-})
+    return res.send(users).status(200);
+});
 
 router.post('/users/find', async (req: Request, res: Response) => {
     const user = await prisma.user.findFirst({
@@ -18,33 +18,33 @@ router.post('/users/find', async (req: Request, res: Response) => {
             email: req.body.email,
             password: req.body.password
         }
-    })
+    });
 
-    if (!user) return res.json({ Error: 'User not found' }).status(200)
+    if (!user) return res.send({ Error: 'User not found' }).status(200);
 
-    return res.json({ User: user }).status(200)
-})
+    return res.send({ User: user }).status(200);
+});
 
 router.post('/users/new', async (req: Request, res: Response) => {
     const user = await prisma.user.findFirst({
         where: {
             email: req.body.email
         }
-    })
+    });
 
-    if (user) return res.json({ Error: "Email alreads in use" }).status(401)
+    if (user) return res.send({ Error: "Email alreads in use" }).status(401);
 
     const newUser = await prisma.user.create({
         data: req.body
     });
     
-    if (!newUser) return res.json({ Error: "User is not created" }).status(500)
+    if (!newUser) return res.send({ Error: "User is not created" }).status(201);
 
-    return res.json({ User: newUser }).status(200)
-})
+    return res.send({ User: newUser }).status(200);
+});
 
 router.put('/users/update', async (req: Request, res: Response) => {
-    const { oldEmail, newEmail } = req.body
+    const { oldEmail, newEmail } = req.body;
 
     const updatedUser = await prisma.user.update({
         where: {
@@ -53,10 +53,10 @@ router.put('/users/update', async (req: Request, res: Response) => {
         data: {
             email: newEmail
         }
-    })
+    });
 
-    return res.json(updatedUser).status(200)
-})
+    return res.send(updatedUser).status(200);
+});
 
 router.delete('/users/remove', async (req: Request, res: Response) => {
     const { email } = req.body
@@ -65,9 +65,9 @@ router.delete('/users/remove', async (req: Request, res: Response) => {
         where: {
             email: email
         }
-    })
+    });
 
-    return res.json({ DeletedUser: deletedUser }).status(200)
+    return res.send({ DeletedUser: deletedUser }).status(200);
 })
 
 export default router;
