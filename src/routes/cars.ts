450c80a9be9b29c8d11aa9bdb7040e045d894bb8
lel-09 <willianandrade953@gmail.com>
2023-04-17 14:38:54 -0300

    get cars route

--- src/routes/cars.ts
+++ src/routes/cars.ts
@@ -4,7 +4,15 @@ import { Router, Request, Response } from "express";
 const prisma = new PrismaClient();
 const router = Router();
 
-router.post('/car/new', async (req: Request, res: Response) => {
+router.get('/cars/all', async (req: Request, res: Response) => {
+    const cars = await prisma.car.findMany()
+
+    if (cars.length === 0) return res.send({ Error: 'User not found' }).status(404)
+
+    return res.send({ Cars: cars }).status(200)
+})
+
+router.post('/cars/new', async (req: Request, res: Response) => {
     const newCar = await prisma.car.create({
         data: req.body
     });
