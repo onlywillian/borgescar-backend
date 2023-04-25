82bf5885caf17771ce47b39049f91abc45c87615
lel-09 <willianandrade953@gmail.com>
2023-04-25 10:32:02 -0300

    Array of images in car collection

--- src/routes/cars.ts
+++ src/routes/cars.ts
@@ -7,9 +7,23 @@ const router = Router();
 router.get('/cars/all', async (req: Request, res: Response) => {
     const cars = await prisma.car.findMany()
 
-    if (cars.length === 0) return res.send({ Error: 'User not found' }).status(404)
+    if (cars.length === 0) return res.send({ Error: 'No cars found' }).status(404)
 
     return res.send({ Cars: cars }).status(200)
+})
+
+router.post('/cars/find', async (req: Request, res: Response) => {
+    if (!req.body.id) return res.send({ Error: 'Id not provided' }).status(401)
+
+    const car = await prisma.car.findFirst({
+        where: {
+            id: req.body.id
+        }
+    });
+    
+    if (!car) return res.json({ Error: "Car is not created" }).status(500)
+
+    return res.json({ Car: car }).status(200)
 })
 
 router.post('/cars/new', async (req: Request, res: Response) => {
@@ -17,9 +31,9 @@ router.post('/cars/new', async (req: Request, res: Response) => {
         data: req.body
     });
     
-    if (!newCar) return res.json({ Error: "User is not created" }).status(500)
+    if (!newCar) return res.json({ Error: "Car is not created" }).status(500)
 
-    return res.json({ User: newCar }).status(200)
+    return res.json({ NewCar: newCar }).status(200)
 })
 
 export default router;
