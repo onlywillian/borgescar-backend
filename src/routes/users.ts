b3cc7c07244d904a211e8d9eb3006b776d10637e
onlywillian <willianandrade953@gmail.com>
2023-04-27 14:26:27 -0300

    Removing .env file

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import authMiddleware from "../middlewares/authMiddleware";
 
 const router = Router();
 const prisma = new PrismaClient();
@@ -13,49 +14,61 @@ router.get("/users/all", async (req: Request, res: Response) => {
   return res.send(users).status(200);
 });
 
-router.post("/users/new", async (req: Request, res: Response) => {
-  const user = await prisma.user.findFirst({
-    where: {
-      email: req.body.email,
-    },
-  });
+router.post(
+  "/users/new",
+  authMiddleware,
+  async (req: Request, res: Response) => {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: req.body.email,
+      },
+    });
 
-  if (user) return res.send({ Error: "Email alreads in use" }).status(401);
+    if (user) return res.send({ Error: "Email alreads in use" }).status(401);
 
-  const newUser = await prisma.user.create({
-    data: req.body,
-  });
+    const newUser = await prisma.user.create({
+      data: req.body,
+    });
 
-  if (!newUser) return res.send({ Error: "User is not created" }).status(201);
+    if (!newUser) return res.send({ Error: "User is not created" }).status(201);
 
-  return res.send({ User: newUser }).status(200);
-});
+    return res.send({ User: newUser }).status(200);
+  }
+);
 
-router.put("/users/update", async (req: Request, res: Response) => {
-  const { oldEmail, newEmail } = req.body;
+router.put(
+  "/users/update",
+  authMiddleware,
+  async (req: Request, res: Response) => {
+    const { oldEmail, newEmail } = req.body;
 
-  const updatedUser = await prisma.user.update({
-    where: {
-      email: oldEmail,
-    },
-    data: {
-      email: newEmail,
-    },
-  });
+    const updatedUser = await prisma.user.update({
+      where: {
+        email: oldEmail,
+      },
+      data: {
+        email: newEmail,
+      },
+    });
 
-  return res.send(updatedUser).status(200);
-});
+    return res.send(updatedUser).status(200);
+  }
+);
 
-router.delete("/users/remove", async (req: Request, res: Response) => {
-  const { email } = req.body;
+router.delete(
+  "/users/remove",
+  authMiddleware,
+  async (req: Request, res: Response) => {
+    const { email } = req.body;
 
-  const deletedUser = await prisma.user.delete({
-    where: {
-      email: email,
-    },
-  });
+    const deletedUser = await prisma.user.delete({
+      where: {
+        email: email,
+      },
+    });
 
-  return res.send({ DeletedUser: deletedUser }).status(200);
-});
+    return res.send({ DeletedUser: deletedUser }).status(200);
+  }
+);
 
 export default router;
